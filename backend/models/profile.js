7d00d0bdf17331401f3f26ae03852e5ec2d395df
anishakatkade <katkadeanisha@gmail.com>
2026-04-06 21:44:51 +0530

    fix profile path

--- backend/models/profile.js
+++ backend/models/profile.js
@@ -1,35 +1,12 @@
 import mongoose from "mongoose";
-const profileSchema = new mongoose.Schema(
-  {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    bio: {
-      type: String,
-      default: "",
-    },
-    college: {
-      type: String,
-    },
-    company: {
-      type: String,
-    },
-    linkedin: {
-      type: String,
-    },
-    github: {
-      type: String,
-    },
-    profilePic: {
-      type: String,
-      default: "",
-    },
-  },
-  { timestamps: true },
-);
 
-const Profile =
-  mongoose.models.Profile || mongoose.model("Profile", profileSchema);
-export default Profile;
+const profileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  bio: String,
+  skills: [String]
+});
+
+export default mongoose.model("Profile", profileSchema);
