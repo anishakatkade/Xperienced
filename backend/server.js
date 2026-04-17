1f1fd938753b66cbce4dfa6607c344ac70f3da2f
anishakatkade <katkadeanisha@gmail.com>
2026-04-17 13:36:14 +0530

    env setup fixed

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: [
+      "http://localhost:5173",
+      "https://xperienced-eight.vercel.app",
+    ],
   },
 });
 
--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_BACKEND_URL);
+const socket = io("https://xperienced.onrender.com", {
+  transports: ["websocket"],
+});
 
 export default socket;
